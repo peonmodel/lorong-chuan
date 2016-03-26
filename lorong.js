82a71a46b6784d1eeea404417fc2359ec6f263ae
Ong Jun Yong <junyong@validatis.com>
2016-03-26 12:57:10 +0800

    separated layout from Lobby

--- lorong.js
+++ lorong.js
@@ -1,53 +1,13 @@
+_ = lodash;
+
 Players = new Mongo.Collection('Players');
 Messages = new Mongo.Collection('Messages');
 Rooms = new Mongo.Collection('Rooms');
 
 if (Meteor.isClient) {
 	
-  Session.setDefault('session', 'user_'+Random.id(4));
+	Session.setDefault('session', 'user_'+Random.id(4));
 	Session.setDefault('sessionUUID', Random.id());
-	
-	Template['layout-main'].events({
-		'click button.register'(event){
-			let instance = Template.instance();
-			let registerAs = $(instance.find('input.loginAs')).val();
-			console.log('registerAs', registerAs);
-			Accounts.createUser({
-				username: registerAs,
-//				email: '',
-				password: Session.get('sessionUUID'),
-				profile: {
-//					date_created: new Date(),
-//					last_active: new Date(),
-//					registered: false,  // is temp
-				},
-			}, (err)=>{
-				console.log(err);
-//				sAlert()
-			});			
-		},
-		'click button.login'(event){
-			let instance = Template.instance();
-			let loginAs = $(instance.find('input.loginAs')).val();
-			console.log('loginAs', loginAs);
-			Meteor.loginWithPassword(
-				{username: loginAs}, 
-				Session.get('sessionUUID'), 
-				(err)=>{
-					console.log('log in', err)
-				}
-			);
-		},
-		'click button.logout'(event){
-			Meteor.logout((err)=>{
-				if (err) {
-					sAlert.error(`error logging out`);
-				} else {
-					sAlert.success(`logged out`);
-				}
-			});
-		},
-	});
 	
 	Template.Lobby.onCreated(function(){
 		let instance = this;

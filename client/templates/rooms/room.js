560c1227ea2fc4711659a395636bf8f30c3ccf9b
Anima-t3d <wings_of_apollo@belgacom.net>
2016-03-27 13:01:29 +0800

    Collection fixes + added room subscription and indication of number of players in the room

--- client/templates/rooms/room.js
+++ client/templates/rooms/room.js
@@ -1,40 +1,59 @@
 Template.Room.onRendered(function () {
 	let instance = this;
-	console.log(instance)
-	instance.accesscode = FlowRouter.getParam('accesscode');
-	instance.user = Session.get('session');
-	let taken = Players.findOne({
-		name: instance.user,
-		room: instance.accesscode,
+	instance.autorun(function (c) {
+		instance.subscribe('Rooms');
+		if(instance.subscriptionsReady()) {
+			instance.accesscode = FlowRouter.getParam('accesscode');
+			instance.user = Session.get('session');
+			let taken = Players.findOne({
+				name: instance.user,
+				room: instance.accesscode,
+			});
+			if (!!taken) {
+				let new_user = instance.user + Random.id(4);
+				sAlert.error(`username ${instance.user} already taken, renaming to ${new_user}`);
+				Session.set('session', new_user);
+				instance.user = new_user;
+			}
+			Players.insert({
+				name: instance.user,
+				room: instance.accesscode,
+			});
+			c.stop();
+		}
 	});
-	if (!!taken) {
-		let new_user = instance.user + Random.id(4);
-		sAlert.error(`username ${instance.user} already taken, renaming to ${new_user}`);
-		Session.set('session', new_user);
-		instance.user = new_user;
-	}
-	Players.insert({
-		name: instance.user,
-		room: instance.accesscode,
-	});
-	//		instance.subscribe();
+
 });
 Template.Room.helpers({
 	accesscode() {
-			let instance = Template.instance();
-			return instance.accesscode;
-		},
-		messages() {
-			let instance = Template.instance();
+		let instance = Template.instance();
+		return instance.accesscode;
+	},
+	messages() {
+		let instance = Template.instance();
 		return Message.collection.find({
-				room_id: instance.accesscode
-			}, {
-				sort: {
-					timestamp: 1
-				},
-				limit: 5,
-			});
-		}
+			room_id: instance.accesscode
+		}, {
+			sort: {
+				timestamp: 1
+			},
+			limit: 5,
+		});
+	},
+	occupancy() {
+		let instance = Template.instance();
+		let foundRoom = Room.collection.findOne({
+			_id: instance.accesscode
+		});
+		return foundRoom && foundRoom.occupancy;
+	},
+	capacity() {
+		let instance = Template.instance();
+		let foundRoom = Room.collection.findOne({
+			_id: instance.accesscode
+		});
+		return foundRoom && foundRoom.capacity;
+	}
 });
 
 Template.Room.events({
